077ba73e9d69f7bf127e88cd576b1f809d6616fd
bergad37 <bergad37@gmail.com>
2026-01-05 16:00:08 +0200

    validate create and update customer

--- src/validations/customer.validation.ts
+++ src/validations/customer.validation.ts
@@ -6,18 +6,18 @@ export const createCustomerValidation = [
     .withMessage('Customer name is required')
     .isLength({ min: 2, max: 100 })
     .withMessage('Customer name must be between 2 and 100 characters'),
-  body('email')
-    .optional()
-    .isEmail()
-    .withMessage('Please provide a valid email'),
-  body('phone')
-    .optional()
-    .isMobilePhone('any')
-    .withMessage('Please provide a valid phone number'),
-  body('address')
-    .optional()
-    .isLength({ min: 5, max: 255 })
-    .withMessage('Address must be between 5 and 255 characters'),
+  //   body('email')
+  //     .optional()
+  //     .isEmail()
+  //     .withMessage('Please provide a valid email'),
+  //   body('phone')
+  //     .optional()
+  //     .isMobilePhone('any')
+  //     .withMessage('Please provide a valid phone number'),
+  //   body('address')
+  //     .optional()
+  //     .isLength({ min: 5, max: 255 })
+  //     .withMessage('Address must be between 5 and 255 characters'),
 ];
 
 export const updateCustomerValidation = [
@@ -27,16 +27,16 @@ export const updateCustomerValidation = [
     .withMessage('Customer name cannot be empty')
     .isLength({ min: 2, max: 100 })
     .withMessage('Customer name must be between 2 and 100 characters'),
-  body('email')
-    .optional()
-    .isEmail()
-    .withMessage('Please provide a valid email'),
-  body('phone')
-    .optional()
-    .isMobilePhone('any')
-    .withMessage('Please provide a valid phone number'),
-  body('address')
-    .optional()
-    .isLength({ min: 5, max: 255 })
-    .withMessage('Address must be between 5 and 255 characters'),
+  //   body('email')
+  //     .optional()
+  //     .isEmail()
+  //     .withMessage('Please provide a valid email'),
+  //   body('phone')
+  //     .optional()
+  //     .isMobilePhone('any')
+  //     .withMessage('Please provide a valid phone number'),
+  //   body('address')
+  //     .optional()
+  //     .isLength({ min: 5, max: 255 })
+  //     .withMessage('Address must be between 5 and 255 characters'),
 ];
